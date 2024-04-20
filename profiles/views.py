d3d1e4662ab710ad2a11f5940f662a3e01d69211
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-20 19:15:15 +0000

    Add view and url to get a single profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -11,4 +12,21 @@ class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    Returns a single profile.
+    """
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
