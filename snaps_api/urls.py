50defc96e55f160cad637ceb050120b602b03c25
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-20 19:38:27 +0000

    Add path for login to urls.py

--- snaps_api/urls.py
+++ snaps_api/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
 ]
