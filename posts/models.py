28dca19c4ab944714750185adddfef55dcb25afb
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-22 10:25:33 +0000

    Add Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Post model related to User.
+    A default image is used if no image is provided.
+    """
+    post_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_mzrjun', blank=True
+        )
+    tags = TaggableManager()
+    download_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
