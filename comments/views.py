58435dd2efe28aa903976d470f70e87559730b40
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-23 09:57:29 +0000

    Add CommentList to view comments

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from snaps_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    Returns a list of all comments.
+    A comment can be created by an authenticated user.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
