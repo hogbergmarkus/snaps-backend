a3dbe6515edde1200733f3f60c868bbad946cfc0
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-23 10:02:45 +0000

    Add urls for CommentDetail

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('comments/', views.CommentList.as_view()),
+    path('comments/<int:pk>/', views.CommentDetail.as_view()),
 ]
