9fbc84cb412a623b7a66eb380f681290df9939cc
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-23 10:31:15 +0000

    Refactor PostList and PostDetail views to use generics

--- posts/views.py
+++ posts/views.py
@@ -1,86 +1,27 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, permissions
+from rest_framework import generics, permissions
 from .models import Post
-from snaps_api.permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer
+from snaps_api.permissions import IsOwnerOrReadOnly
 
 
-class PostList(APIView):
+class PostList(generics.ListCreateAPIView):
     """
     Returns a list of all posts.
     A post can be created by an authenticated user.
     """
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(
-            posts,
-            many=True,
-            context={'request': request}
-            )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = PostSerializer(
-            data=request.data,
-            context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-                )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-            )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class PostDetail(APIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Returns a single post.
-    Get post or return 404 if not found.
-    IsOwnerOrReadOnly allows only the object owner to edit it.
-    The put method allows you to update a post.
-    The delete method allows you to delete a post.
+    Provides the details for a single post,
+    update and delete it if you own it.
     """
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
