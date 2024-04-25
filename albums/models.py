5eec921a23b52d14221b3d891a346c096083066f
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-25 10:47:07 +0000

    Add Album model to albums app

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Album(models.Model):
+    """
+    Album model related to User and Post.
+    """
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='albums',
+        )
+    title = models.CharField(max_length=255, blank=False, default='New Album')
+    posts = models.ManyToManyField(Post, related_name='albums')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
