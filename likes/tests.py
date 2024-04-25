4e726ed34afeee57692775232fd23f4228015ac7
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-25 10:00:52 +0000

    Add tests for LikeDetail view

--- likes/tests.py
+++ likes/tests.py
@@ -75,3 +75,51 @@ class LikeListViewTests(APITestCase):
         post = Post.objects.get(title='this is a title')
         response = self.client.post('/likes/', {'post': post.id})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class LikeDetailViewTests(APITestCase):
+    """
+    Tests for the Like detail view.
+    Like can be deleted from a post or comment by its owner.
+    Like can not be deleted from a post or comment by an unauthenticated user.
+    An owners Like can not be deleted by another user.
+    """
+    def setUp(self):
+        self.adam = User.objects.create_user(username='adam', password='pass')
+        self.brian = User.objects.create_user(
+            username='brian', password='pass'
+            )
+        self.post = Post.objects.create(
+            owner=self.brian, title='this is a title'
+            )
+        self.comment = Comment.objects.create(
+            owner=self.adam, post=self.post, content='this is a comment'
+            )
+
+    def test_owner_can_delete_like_from_post(self):
+        self.client.login(username='adam', password='pass')
+        like = Like.objects.create(owner=self.adam, post=self.post)
+        response = self.client.delete(f'/likes/{like.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_owner_can_delete_like_from_comment(self):
+        self.client.login(username='brian', password='pass')
+        like = Like.objects.create(owner=self.brian, comment=self.comment)
+        response = self.client.delete(f'/likes/{like.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_unauthenticated_user_cannot_delete_like_from_comment(self):
+        like = Like.objects.create(owner=self.brian, comment=self.comment)
+        response = self.client.delete(f'/likes/{like.id}/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_unauthenticated_user_cannot_delete_like_from_post(self):
+        like = Like.objects.create(owner=self.adam, post=self.post)
+        response = self.client.delete(f'/likes/{like.id}/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_owners_like_cannot_be_deleted_by_another_user(self):
+        self.client.login(username='brian', password='pass')
+        like = Like.objects.create(owner=self.adam, post=self.post)
+        response = self.client.delete(f'/likes/{like.id}/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
