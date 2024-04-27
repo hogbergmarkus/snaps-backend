e5d81af6ea4a109f0377e6c141e44a3bc219d58b
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-27 10:38:04 +0000

    Add test for ProfileList view

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class ProfileListViewTests(APITestCase):
+    """
+    Tests for the Profile list view.
+    Profiles can be listed.
+    """
+    def setUp(self):
+        self.adam = User.objects.create_user(username='adam', password='pass')
+
+    def test_can_list_profiles(self):
+        response = self.client.get('/profiles/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
