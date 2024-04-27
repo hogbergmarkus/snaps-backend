bcb40beda07785fcf78e5f0b79aabe62273a454f
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-27 11:22:05 +0000

    Add tests for ProfileDetail view

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
+from .models import Profile
 from rest_framework import status
 
 
@@ -14,3 +15,46 @@ class ProfileListViewTests(APITestCase):
     def test_can_list_profiles(self):
         response = self.client.get('/profiles/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ProfileDetailViewTests(APITestCase):
+    """
+    Tests for the Profile detail view.
+    Profiles can be retrieved.
+    Profiles can only be updated by their owner.
+    """
+    def setUp(self):
+        self.adam = User.objects.create_user(username='adam', password='pass')
+        self.adam_profile = Profile.objects.get(owner=self.adam)
+        self.brian = User.objects.create_user(
+            username='brian', password='pass'
+            )
+
+    def test_can_retrieve_profile(self):
+        response = self.client.get(f'/profiles/{self.adam_profile.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_owner_can_update_profile(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put(
+            f'/profiles/{self.adam_profile.id}/',
+            {'username': 'mada'}
+            )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.adam.refresh_from_db()
+        self.assertEqual(self.adam.profile.username, 'mada')
+
+    def test_logged_out_user_cannot_update_profile(self):
+        response = self.client.put(
+            f'/profiles/{self.adam_profile.id}/',
+            {'username': 'mada'}
+            )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_in_user_cannot_update_other_users_profile(self):
+        self.client.login(username='brian', password='pass')
+        response = self.client.put(
+            f'/profiles/{self.adam_profile.id}/',
+            {'username': 'mada'}
+            )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
