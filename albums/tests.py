4e19a8ad90b95ea1e6448e7a07e8757839a42b08
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-27 08:39:46 +0000

    Add tests for AlbumDetail view

--- albums/tests.py
+++ albums/tests.py
@@ -43,3 +43,58 @@ class AlbumListViewTests(APITestCase):
         response = self.client.get('/albums/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, [])
+
+
+class AlbumDetailViewTests(APITestCase):
+    """
+    Tests for the Album detail view.
+    Users can retrieve details of a single album.
+    Users can only update or delete their own albums.
+    """
+    def setUp(self):
+        self.adam = User.objects.create_user(username='adam', password='pass')
+        self.brian = User.objects.create_user(
+            username='brian', password='pass'
+            )
+        self.album = Album.objects.create(owner=self.adam, title='My Album')
+
+    def test_can_retrieve_a_single_album(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.get(f'/albums/{self.album.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_update_album(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put(
+            f'/albums/{self.album.id}/', {'title': 'New Title'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.album.refresh_from_db()
+        self.assertEqual(self.album.title, 'New Title')
+
+    def test_logged_in_user_can_delete_album(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.delete(f'/albums/{self.album.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_logged_out_user_cannot_update_album(self):
+        response = self.client.put(
+            f'/albums/{self.album.id}/', {'title': 'New Title'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_out_user_cannot_delete_album(self):
+        response = self.client.delete(f'/albums/{self.album.id}/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_in_user_cannot_update_other_users_album(self):
+        self.client.login(username='brian', password='pass')
+        response = self.client.put(
+            f'/albums/{self.album.id}/', {'title': 'New Title'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_logged_in_user_cannot_delete_other_users_album(self):
+        self.client.login(username='brian', password='pass')
+        response = self.client.delete(f'/albums/{self.album.id}/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
