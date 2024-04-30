4e0a44ab793b1221abee5804b2720ee867742a56
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-30 08:53:31 +0000

    Set default renderer class to JSONRenderer

--- snaps_api/settings.py
+++ snaps_api/settings.py
@@ -37,6 +37,11 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
 REST_AUTH = {
     'USE_JWT': True,
     'JWT_AUTH_SECURE': True,
