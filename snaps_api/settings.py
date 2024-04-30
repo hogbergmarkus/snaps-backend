418d88b18c233036ab6f65e9091a5019df4699cd
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-30 10:42:07 +0000

    Try correcting allowed host again

--- snaps_api/settings.py
+++ snaps_api/settings.py
@@ -63,7 +63,7 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-hogbergmark-snapsbacken-o3kkxq6c3ir.ws-eu110.gitpod.io',
-    'https://snaps-2a8aec1abfbd.herokuapp.com',
+    'snaps-2a8aec1abfbd.herokuapp.com',
 ]
 
 
