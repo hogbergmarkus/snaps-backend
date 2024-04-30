39f37e6dc59ab96b459a10fcbba25616923e7d99
Markus Högberg <m.o.hogberg@gmail.com>
2024-04-30 09:10:21 +0000

    Add method to display natural time for comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 from likes.models import Like
@@ -9,13 +10,14 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner, profile_id and profile_image are added
     when returning a list of Comment instances.
     """
-
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -43,6 +45,12 @@ class CommentSerializer(serializers.ModelSerializer):
         through related_name="likes".
         """
         return obj.likes.count()
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         model = Comment
