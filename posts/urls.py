86ff8923737cb660fec25d01c8c0a1be8268e8df
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-01 12:25:31 +0000

    Add url for incrementing download count on posts

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
     path('posts/<int:pk>/', views.PostDetail.as_view()),
+    path('posts/<int:pk>/download/', views.IncrementDownloadCount.as_view()),
 ]
