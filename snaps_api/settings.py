015063800b7d44dcc4c834d13a2fe61dc9c7e3b8
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-09 20:49:57 +0000

    Add new default authentication class

--- snaps_api/settings.py
+++ snaps_api/settings.py
@@ -31,7 +31,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
-        else 'rest_framework_simplejwt.authentication.JWTAuthentication'
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
