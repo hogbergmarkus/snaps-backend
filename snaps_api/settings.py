bcf77bbddeaeb967faec7d85f03009e1e193affa
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-09 20:54:16 +0000

    Trying to fix deployment issue

--- snaps_api/settings.py
+++ snaps_api/settings.py
@@ -31,7 +31,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
-        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
