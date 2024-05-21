3cbb6b6a87f0e04bf7aab056041a6e34ab9cbaa7
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-21 16:15:58 +0000

    Register Profile in admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import Profile
 
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Register Profile model in the admin panel
+    """
+    list_display = (
+        'owner',
+        'username',
+        'created_at',
+        'updated_at',
+        'image',
+    )
+    list_filter = (
+        'username',
+        'owner',
+    )
