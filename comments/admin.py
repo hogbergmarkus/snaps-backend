6b2014616749fd9cce9cad86f6e74dd099549ba3
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-21 16:34:28 +0000

    Add Comment model to admin panel

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Register Comment model in the admin panel
+    """
+    list_display = (
+        'owner',
+        'post',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'owner',
+        'post',
+        'updated_at',
+    )
