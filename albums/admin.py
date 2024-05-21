5876a3096f786b388cf10757b9f0e8e6c1e152cc
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-21 16:38:28 +0000

    Add Album model to admin panel

--- albums/admin.py
+++ albums/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Album
 
-# Register your models here.
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    """
+    Register Album model in the admin panel
+    """
+    list_display = (
+        'owner',
+        'title',
+        'updated_at',
+    )
+    list_filter = (
+        'owner',
+        'updated_at',
+    )
