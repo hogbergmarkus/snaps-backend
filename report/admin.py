9713bbb54e8d1dc6f4e9da6c1763b5a01fc15ece
Markus Högberg <m.o.hogberg@gmail.com>
2024-06-04 16:44:07 +0000

    Add report to admin panel

--- report/admin.py
+++ report/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Report
 
-# Register your models here.
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    """
+    Register Report model in the admin panel
+    """
+    list_display = (
+        'user',
+        'post',
+        'reported_at',
+    )
+    list_filter = (
+        'user',
+        'post',
+    )
