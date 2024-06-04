8b47b4d383ddbcd7b3943059ea13024353e973d1
Markus Högberg <m.o.hogberg@gmail.com>
2024-06-04 16:43:00 +0000

    Add view for the report model

--- report/views.py
+++ report/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Report
+from .serializers import ReportSerializer
 
-# Create your views here.
+
+class ReportList(generics.ListCreateAPIView):
+    """
+    Returns a list of all reports.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
