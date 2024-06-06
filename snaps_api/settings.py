c6e2579ff8c12577005cfe3d024bd0c81760f57f
Markus Högberg <m.o.hogberg@gmail.com>
2024-06-06 23:49:40 +0000

    Set debug to hard False

--- snaps_api/settings.py
+++ snaps_api/settings.py
@@ -60,7 +60,7 @@ REST_AUTH = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
